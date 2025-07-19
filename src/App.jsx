b284acd2fcc192dfe64b103c9b095d0925606c00
Sudeepti Gupta <sudeeptigupta111@gmail.com>
2025-07-19 21:39:55 +0530

    Add assignment 55.1 files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Table from "./Table";
+
+
+function App() {
+
+
+  return (
+    <div className="flex gap-4">
+    <Table></Table>
+    <Table></Table>
+    </div>
+   
+  );
+}
+
+export default App
