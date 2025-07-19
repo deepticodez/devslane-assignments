ad4fed49fe87ceaa6fe23a8a1134d06698d7a8cb
Sudeepti Gupta <sudeeptigupta111@gmail.com>
2025-07-19 22:58:37 +0530

    Add assignment 55.2 files

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  
+
+  return (
+    <div className=" h-20 bg-white pt-2">
+      <img className="h-17 pl-40 " src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo.png"/>
+      
+    </div>
+  );
+}
+
+export default Header
