fc3a7c152cc0f00bebe1928100f766adfebd6758
Sudeepti Gupta <sudeeptigupta111@gmail.com>
2025-08-16 17:18:57 +0530

    Add assignment 58 files

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import ProductList from "./ProductList";
+import ProductListPage from "./ProductListPage";
+
+
+import Sidebar from "./Sidebar";
+
+import ProductDescription from "./ProductDesciption";
+
+function App() {
+  const path = window.location.pathname;
+  console.log("path is", path);
+
+  return (
+    <div className="max-w-full bg-gray-200 min-h-screen">
+      <Header/>
+      <Routes>
+        <Route index element={<ProductListPage />}></Route>
+        <Route
+          path="/products/1"
+          element={<ProductDescription />}
+        ></Route>
+      </Routes>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
+
+/*<div className="flex">
+  <div className="w-32 h-screen bg-gray-500 p-2 flex flex-col space-y-2">
+    <a href="/assignments">Assignemnts</a>
+    <a href="/lectures">Lecture</a>
+  </div>
+  {path == "/assignments" && <AssignmentPage />}
+  {path == "/lectures" && <LecturePage />}
+</div>;*/
